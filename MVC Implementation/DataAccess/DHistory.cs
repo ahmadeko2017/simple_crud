f34d7af588946ba428db596cb17c2f827522550f
ahmadeko2017 <44262212+ahmadeko2017@users.noreply.github.com>
2023-07-13 19:21:32 +0700

    Menambahkan data access untuk history

---
+++ MVC Implementation/DataAccess/DHistory.cs
@@ -0,0 +1,155 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using MVC_Implementation.Models;
+
+namespace MVC_Implementation.DataAccess;
+
+public class DHistory
+{
+    public History SelectById(DateTime startDate, int employeeId)
+    {
+        History result = new History(DateTime.Now, 0, DateTime.Now, 0, "");
+        var conStr = Connection.Get();
+        using var connection = new SqlConnection(conStr);
+        connection.Open();
+        try
+        {
+            string query = "SELECT * FROM histories WHERE start_date = @startDate AND employee_id = @employeeId";
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
+            sqlCommand.Parameters.AddWithValue("@startDate", startDate);
+            sqlCommand.Parameters.AddWithValue("@employeeId", employeeId);
+            using SqlDataReader reader = sqlCommand.ExecuteReader();
+                
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    result = new History(reader.GetDateTime("start_date"), reader.GetInt32("employee_id"), reader.GetDateTime("end_date"), reader.GetInt32("department_id"), reader.GetString("job_id"));
+                }
+            }
+            reader.Close();
+        }
+        catch
+        {
+            // ignored
+        }
+        finally
+        {
+            connection.Close();
+        }
+        return result;
+    }
+
+    public List<History> SelectAll()
+    {
+        List<History> results = new List<History>();
+        var conStr = Connection.Get();
+        using var connection = new SqlConnection(conStr);
+        connection.Open();
+        try
+        {
+            string query = "SELECT * FROM histories";
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
+            using SqlDataReader reader = sqlCommand.ExecuteReader();
+                
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    results.Add(new History(reader.GetDateTime("start_date"), reader.GetInt32("employee_id"), reader.GetDateTime("end_date"), reader.GetInt32("department_id"), reader.GetString("job_id")));
+                }
+            }
+            reader.Close();
+        }
+        catch
+        {
+            // ignored
+        }
+        finally
+        {
+            connection.Close();
+        }
+        return results;
+    }
+
+    public int Insert(History history)
+    {
+        int result = 0;
+        var conStr = Connection.Get();
+        using var connection = new SqlConnection(conStr);
+        connection.Open();
+        try
+        {
+            string query = "INSERT INTO histories VALUES (@startDate, @employeeId, @endDate, @departmentId, @jobId)";
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
+            sqlCommand.Parameters.AddWithValue("@startDate", history.StartDate);
+            sqlCommand.Parameters.AddWithValue("@employeeId", history.EmployeeId);
+            sqlCommand.Parameters.AddWithValue("@endDate", history.EndDate);
+            sqlCommand.Parameters.AddWithValue("@departmentId", history.DepartmentId);
+            sqlCommand.Parameters.AddWithValue("@jobId", history.JobId);
+            result = sqlCommand.ExecuteNonQuery();
+        }
+        catch
+        {
+            result = -1;
+        }
+        finally
+        {
+            connection.Close();
+        }
+        return result;
+    }
+
+    public int Update(History history)
+    {
+        int result = 0;
+        var conStr = Connection.Get();
+        using var connection = new SqlConnection(conStr);
+        connection.Open();
+        try
+        {
+            string query = "UPDATE histories SET end_date = @endDate, department_id = @departmentId, job_id = @jobId WHERE start_date = @startDate AND employee_id = @employeeId";
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
+            sqlCommand.Parameters.AddWithValue("@startDate", history.StartDate);
+            sqlCommand.Parameters.AddWithValue("@employeeId", history.EmployeeId);
+            sqlCommand.Parameters.AddWithValue("@endDate", history.EndDate);
+            sqlCommand.Parameters.AddWithValue("@departmentId", history.DepartmentId);
+            sqlCommand.Parameters.AddWithValue("@jobId", history.JobId);
+            result = sqlCommand.ExecuteNonQuery();
+        }
+        catch
+        {
+            result = -1;
+        }
+        finally
+        {
+            connection.Close();
+        }
+        return result;
+    }
+
+    public int Delete(DateTime startDate, int employeeId)
+    {
+        int result = 0;
+        var conStr = Connection.Get();
+        using var connection = new SqlConnection(conStr);
+        connection.Open();
+        try
+        {
+            string query = "DELETE histories WHERE start_date = @startDate AND employee_id = @employeeId";
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
+            sqlCommand.Parameters.AddWithValue("@startDate", startDate);
+            sqlCommand.Parameters.AddWithValue("@employeeId", employeeId);
+            result = sqlCommand.ExecuteNonQuery();
+        }
+        catch
+        {
+            result = -1;
+        }
+        finally
+        {
+            connection.Close();
+        }
+        return result;
+    }
+}
